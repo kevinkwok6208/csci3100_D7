68f03ab64f058b4d6ed72b3368214a9a3f4ec5b7
kevinkwok6208 <kevinkwok6208@gmail.com>
2025-03-23 02:23:21 +0800

    Update route in ProductDisplay

--- backend/routes/productDisplay.js
+++ backend/routes/productDisplay.js
@@ -18,16 +18,16 @@ router.get('/get-allproducts', ProductDisplayController.getAllProducts);
 */
 router.get('/get-products/:productId', ProductDisplayController.getProductById);
 
-/*  "http://localhost:5001/api/admin/get-comments/{productId}"
-    "https://localhost:5443/api/admin/get-comments/{productId}"
+/*  "http://localhost:5001/api/product-display/get-comments/{productId}"
+    "https://localhost:5443/api/product-display/get-comments/{productId}"
     Parameters: req.params.productId
     Function: Allow user to view comments for a product.
     Method: Get
 */
 router.get('/get-comments/:productId', ProductDisplayController.getCommentsForProduct);
 
-/*  "http://localhost:5001/api/admin/get-ratings/{productId}"
-    "https://localhost:5443/api/admin/get-ratings/{productId}"
+/*  "http://localhost:5001/api/product-display/get-ratings/{productId}"
+    "https://localhost:5443/api/product-display/get-ratings/{productId}"
     Parameters: req.params.productId
     Function: Allow user to view ratings for a product.
     Method: Get
