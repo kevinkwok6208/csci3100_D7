7b7c0d9db5784a09b4a6042115947b227f3a3fff
kevinkwok6208 <kevinkwok6208@gmail.com>
2025-04-17 19:28:19 +0800

    Add localhost mongoDB path to comment

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
  * @returns {Promise} MongoDB connection promise
  */
 function connectDatabase() {
+  /*mongodb://localhost:27017/CSCI3100_D7*/ 
   return mongoose.connect('mongodb+srv://csci3100_d7:csci3100_d7@csci3100d7.3chef.mongodb.net/CSCI3100_D7', {
     useNewUrlParser: true,
     useUnifiedTopology: true
